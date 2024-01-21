346168de7a77a77a440dc8fcb29d1bb346989300
Subhash <subhashmishra4322@gmail.com>
2024-01-21 22:46:33 +0530

    Day 3 Completed in 3 Days.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import asabenehImg from './images/asabeneh.jpg';
 console.log("Radhe Radhe...")
 const rootElement = document.getElementById('root')
-const newsShelter=(
+const radheRadhe = <h1>Radhe Radhe...</h1>
+const newsShelter = (
   <div className='news_shelter wrapper'>
     <header className='header'>
       <h1>SUBSCRIBE</h1>
@@ -15,10 +16,55 @@ const newsShelter=(
         <input type='text' className='lastName' placeholder='Last Name'></input>
         <input type='email' className='email' placeholder='Email'></input>
         <div>
-        <button type='submit' className='subscribeButton'>Subscribe</button></div>
+          <button type='submit' className='subscribeButton'>Subscribe</button></div>
       </form>
     </main>
   </div>
 )
+const profileCardHeader= (
+  <header className='profileCardHeader pro-card-weapper'>
+    <img className='profileCardImage' src={asabenehImg} alt='profile image'/>
+    <div id='profile-name'>
+    <h1>ASABENEH YATAYEH <span>Senior Developer, Finland</span></h1>
+    </div>
+  </header>
+)
+const profileCardMain=(
+  <main className='profileCardMain pro-card-weapper'>
+    <h2>SKILLS</h2>
+  <div className="Skills">
+  <span>HTML</span>
+  <span>CSS</span>
+  <span>Sass</span>
+  <span>JS</span>
+  <span>React</span>
+  <span>Redux</span>
+  <span>Node</span>
+  <span>MongoDB</span>
+  <span>Python</span>
+  <span>Flask</span>
+  <span>Django</span>
+  <span>Numpy</span>
+  <span>Pandas</span>
+  <span>Data Analysis</span>
+  <span>MYSQL</span>
+  <span>GraphQt</span>
+  <span>D3.js</span>
+  <span>Gastsby</span>
+  <span>Docker</span>
+  <span>Heroky</span>
+  <span>Git</span>
+  </div>
+  <div className='profileCardFooter'>
+    <p>Joined on Aug 30 2020</p>
+  </div>
+  </main>
+)
+const profileCard=(
+  <div>
+    {profileCardHeader}
+    {profileCardMain}
+  </div>
+)
 
-ReactDOM.render(newsShelter, rootElement)
+ReactDOM.render(profileCard, rootElement)
